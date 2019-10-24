716192c541e5a5cee7232f945de59cb6b3a8ce28
Justin Kolberg <amd.prophet@gmail.com>
2019-10-23 22:32:57 -0700

    show PATH, LD_LIBRARY_PATH and CPATH
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

--- main.go
+++ main.go
@@ -7,5 +7,8 @@ import (
 
 func main() {
 	fmt.Println("hello world!")
-	fmt.Println("api token: ", os.Getenv("SENSU_API_TOKEN"))
+	fmt.Println("api token:", os.Getenv("SENSU_API_TOKEN"))
+	fmt.Println("path:", os.Getenv("PATH"))
+	fmt.Println("ld_library_path:", os.Getenv("LD_LIBRARY_PATH"))
+	fmt.Println("cpath:", os.Getenv("CPATH"))
 }
