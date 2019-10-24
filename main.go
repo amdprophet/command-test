2b8b96c3436219d6817baca9a0138b41f0801c19
Justin Kolberg <amd.prophet@gmail.com>
2019-10-24 10:56:52 -0700

    add backend urls to output
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

--- main.go
+++ main.go
@@ -7,8 +7,11 @@ import (
 
 func main() {
 	fmt.Println("hello world!")
+
 	fmt.Println("api token:", os.Getenv("SENSU_API_TOKEN"))
-	fmt.Println("refresh token:", os.Getenv("SENSU_REFRESH_TOKEN"))
+	fmt.Println("api refresh token:", os.Getenv("SENSU_API_REFRESH_TOKEN"))
+	fmt.Println("api endpoints:", os.Getenv("SENSU_API_ENDPOINTS"))
+
 	fmt.Println("path:", os.Getenv("PATH"))
 	fmt.Println("ld_library_path:", os.Getenv("LD_LIBRARY_PATH"))
 	fmt.Println("cpath:", os.Getenv("CPATH"))
