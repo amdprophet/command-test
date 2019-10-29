4a7287555476ad667b3def3a263078e095826be9
Justin Kolberg <amd.prophet@gmail.com>
2019-10-29 16:15:22 -0700

    update formatting, add new fields
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

--- main.go
+++ main.go
@@ -8,11 +8,16 @@ import (
 func main() {
 	fmt.Println("hello world!")
 
-	fmt.Println("api token:", os.Getenv("SENSU_API_TOKEN"))
-	fmt.Println("api refresh token:", os.Getenv("SENSU_API_REFRESH_TOKEN"))
-	fmt.Println("api endpoints:", os.Getenv("SENSU_API_ENDPOINTS"))
+	fmt.Println("SENSU_API_URL=", os.Getenv("SENSU_API_URL"))
+	fmt.Println("SENSU_NAMESPACE=", os.Getenv("SENSU_NAMESPACE"))
+	fmt.Println("SENSU_FORMAT=", os.Getenv("SENSU_FORMAT"))
+	fmt.Println("SENSU_ACCESS_TOKEN=", os.Getenv("SENSU_ACCESS_TOKEN"))
+	fmt.Println("SENSU_ACCESS_TOKEN_EXPIRES_AT=", os.Getenv("SENSU_ACCESS_TOKEN_EXPIRES_AT"))
+	fmt.Println("SENSU_REFRESH_TOKEN=", os.Getenv("SENSU_REFRESH_TOKEN"))
+	fmt.Println("SENSU_TRUSTED_CA_FILE=", os.Getenv("SENSU_TRUSTED_CA_FILE"))
+	fmt.Println("SENSU_INSECURE_SKIP_TLS_VERIFY=", os.Getenv("SENSU_INSECURE_SKIP_TLS_VERIFY"))
 
-	fmt.Println("path:", os.Getenv("PATH"))
-	fmt.Println("ld_library_path:", os.Getenv("LD_LIBRARY_PATH"))
-	fmt.Println("cpath:", os.Getenv("CPATH"))
+	fmt.Println("PATH=", os.Getenv("PATH"))
+	fmt.Println("LD_LIBRARY_PATH=", os.Getenv("LD_LIBRARY_PATH"))
+	fmt.Println("CPATH=", os.Getenv("CPATH"))
 }
